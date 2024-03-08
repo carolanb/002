1af1f33118e7e7c5fa7e65dfbcd630d1b20464e5
Jose Alfonso Martinez Ramirez <jo.alfonso.martinez@gmail.com>
2024-03-08 10:48:16 -0600

    CLASS

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+
+
+class Order:
+    def __init__(self, timestamp, bought_at, stop_loss, take_profit, order_type, sold_at=None, is_active=True):
+        self.timestamp = timestamp
+        self.bought_at = bought_at
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
+        self.order_type = order_type
+        self.sold_at = sold_at
+        self.is_active = is_active
+
+
