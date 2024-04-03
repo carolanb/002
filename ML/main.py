9bbc44a4c4089dd2d02812e81f5341f8920ef795
Jose Alfonso Martinez Ramirez <jo.alfonso.martinez@gmail.com>
2024-04-02 19:34:18 -0600

    a

--- ML/main.py
+++ ML/main.py
@@ -6,51 +6,84 @@ from utils.utils import close_position
 from utils.utils import update_portfolio_values
 from utils.utils import execute_sell_order
 
+
 # main
-def perform(data, commission, stop_loss, take_profit):
+def perform(data, rsi_thresholds, bb_window, mm_windows, commission, stop_loss, take_profit):
     initial_cash = 500_000
+    initial_short_cash = 500_000 
     df_results = pd.DataFrame({'gain': [], 'strategy': [], 'orders_executed': []})
     strategy_dfs = {}
 
     original_strategies = ['svc', 'lr', 'xgboost']
     all_combinations = [list(comb) for r in range(1, len(original_strategies) + 1) for comb in itertools.combinations(original_strategies, r)]
 
+    # Nuevo DataFrame para almacenar los valores combinados del portafolio y el efectivo para cada estrategia
     combined_values_df = pd.DataFrame(index=data.index)
 
-    def backtest(strat, data, initial_cash, commission, stop_loss, take_profit):
-        df_buy = pd.DataFrame(index=data.index)
-        df_sell = pd.DataFrame(index=data.index)
-        cash = initial_cash
-        order_count = 0
+    # Define la función backtest dentro de perform.
+    def backtest(strat, data, initial_cash, initial_short_cash, initial_order_count, rsi_thresholds, bb_window, mm_windows, commission, stop_loss, take_profit):  # -----
+        df_sell = pd.DataFrame()
+        df_buy = pd.DataFrame()
+        COMISSION = 0.0025
+        STOP_LOSS = 0.05
+        TAKE_PROFIT = 0.05
+
+
+        cash, short_cash, order_count = initial_cash, initial_short_cash, initial_order_count
         positions = []
         portfolio_values = []
 
-        define_strategies_ml(strat, data[:-len(data)//5], data[-len(data)//5:], df_buy, df_sell)
+        # Suponiendo que strategies_design modifica los df_buy y df_sell
+        define_strategies_ml(strat, data, df_buy, df_sell, rsi_thresholds, bb_window, mm_windows)  # Asegúrate de que esto está correcto -----
+        
 
+        # DataFrame para registrar valores durante el backtesting
+        record_df = pd.DataFrame(index=data.index, columns=['Portfolio Value', 'Cash'])
+
+        # Bucle principal de backtest
         for (idx, row), (_, row_buy), (_, row_sell) in zip(data.iterrows(), df_buy.iterrows(), df_sell.iterrows()):
-            price = row['Close']
-            for position in positions[:]:
+            price = 1 * row.Close
+
+            # Cierre y apertura de posiciones LONG y SHORT
+            for position in positions[:]:  # Copia de la lista para iteración segura
                 if position.is_active:
-                    cash = close_position(price, position, commission, cash)
+                    if position.order_type == 'LONG':
+                        cash = close_position(price, position, COMISSION, cash)
+                        position.is_active = False  # Asegúrate de marcar la posición como no activa
 
-            if row_buy['xgb_buy_signal'] or row_buy['svc_buy_signal'] or row_buy['lr_buy_signal']:
-                cash, order_count = execute_buy_order(row, positions, commission, stop_loss, take_profit, cash, order_count)
+                    elif position.order_type == 'SHORT':
+                        short_cash = close_position(price, position, COMISSION, short_cash)
+                        position.is_active = False  # Asegúrate de marcar la posición como no activa
 
-            if row_sell['xgb_sell_signal'] or row_sell['svc_sell_signal'] or row_sell['lr_sell_signal']:
-                cash, order_count = execute_sell_order(row, positions, commission, stop_loss, take_profit, cash, order_count)
+            # Contar posiciones activas
+            active_positions = sum(1 for position in positions if position.is_active)
 
-            portfolio_values.append(cash)
 
-        return cash, order_count, portfolio_values, df_buy, df_sell
+            # Condiciones para ejecutar órdenes de compra/venta
+            if active_positions < 100 and row_buy.sum() == len(df_buy.columns):
+                cash, order_count = execute_buy_order(row, positions, COMISSION, 1, STOP_LOSS, TAKE_PROFIT, cash, order_count)
 
+            if active_positions < 100 and row_sell.sum() == len(df_sell.columns):
+                short_cash, order_count = execute_sell_order(row, positions, COMISSION, 1, STOP_LOSS, TAKE_PROFIT, short_cash, order_count)
+
+            current_portfolio_value = update_portfolio_values(data, positions, portfolio_values, cash + short_cash, 1)
+            portfolio_values.append(current_portfolio_value)
+
+            record_df.at[idx, 'Portfolio Value'] = current_portfolio_value
+            record_df.at[idx, 'Cash'] = cash + short_cash
+            
+        return cash + short_cash, order_count, record_df, df_buy, df_sell
+
+    # Bucle a través de todas las combinaciones de estrategias
     for strat in all_combinations:
-        final_cash, order_count, portfolio_values, df_buy, df_sell = backtest(
-            strat, data, initial_cash, commission, stop_loss, take_profit)
+        final_cash, order_count, record_df, df_buy, df_sell = backtest(
+            strat, data, initial_cash, initial_short_cash, 0, 
+            rsi_thresholds, bb_window, mm_windows, commission, stop_loss, take_profit)  # -----
 
-        final_value = portfolio_values[-1]
+        final_value = record_df['Portfolio Value'].iloc[-1]
         new_row = pd.DataFrame({'gain': [final_value], 'strategy': [str(strat)], 'orders_executed': [order_count]})
         df_results = pd.concat([df_results, new_row], ignore_index=True)
-        strategy_dfs[str(strat)] = {'df_buy': df_buy, 'df_sell': df_sell, 'portfolio_values': portfolio_values}
-        combined_values_df[str(strat)] = portfolio_values
+        strategy_dfs[str(strat)] = {'df_buy': df_buy, 'df_sell': df_sell, 'records': record_df}
+        combined_values_df[str(strat)] = record_df['Portfolio Value']  # Solo usa 'Portfolio Value' ya que representa la suma total de valores
 
     return df_results, strategy_dfs, combined_values_df
